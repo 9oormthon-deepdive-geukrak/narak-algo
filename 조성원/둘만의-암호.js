3f8eb655966d48a8bf73b093c87bc7fb6d915c6d
조성원 <tjddnjsjo@kakao.com>
2024-08-28 01:42:59 +0900

    [LV1] 둘만의 암호

---
+++ 조성원/둘만의-암호.js
@@ -0,0 +1,16 @@
+/**
+ * alphabets 배열에서 skip 알파벳을 제외하고,
+ * index만큼 밀어서 새로운 문자열을 반환
+ */
+
+const alphabets = "abcdefghijklmnopqrstuvwxyz".split("");
+
+const solution = (s, skip, index) => {
+	const filtered = alphabets.filter((alphabet) => !skip.includes(alphabet));
+
+	return [...s].reduce(
+		(acc, cur) =>
+			acc + filtered[(filtered.indexOf(cur) + index) % filtered.length],
+		"",
+	);
+};
