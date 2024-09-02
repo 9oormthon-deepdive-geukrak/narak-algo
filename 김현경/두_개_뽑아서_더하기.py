598299755329383b1972c37111a0730fee31f6ea
kkyung99 <becky0508@naver.com>
2024-09-02 14:49:13 +0900

    Revert "[LV1] 두 개 뽑아서 더하기"
    This reverts commit d8a21da479368d24f492437632fc896c8bdd4131.

--- 김현경/두_개_뽑아서_더하기.py
+++
@@ -1,9 +0,0 @@
-from itertools import *
-def solution(numbers):
-    answer = []
-    # 2개 숫자 선택하는 모든 조합을 생성
-    numbers_list = list(combinations(numbers, 2))
-    for i in numbers_list:
-        answer.append(sum(i))
-    answer = sorted(set(answer))
-    return answer
