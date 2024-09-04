4fdd9603de16d09b666a2ab1bd4fd46ca575696f
publdaze <publdaze@naver.com>
2024-09-04 14:26:21 +0900

    [LV2] 행렬의 곱셈 [level 2]

---
+++ 김은지/행렬의-곱셈.js
@@ -0,0 +1,15 @@
+function solution(arr1, arr2) {
+  const matrix = [];
+  for (let i = 0; i < arr1.length; i++) {
+    const row = [];
+    for (let j = 0; j < arr2[0].length; j++) {
+      let cell = 0;
+      for (let k = 0; k < arr1[i].length; k++) {
+        cell += arr1[i][k] * arr2[k][j];
+      }
+      row.push(cell);
+    }
+    matrix.push(row);
+  }
+  return matrix;
+}
