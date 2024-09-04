12314b48054f23b9b34cb6793e9cb65b1593b97c
publdaze <publdaze@naver.com>
2024-09-04 14:57:26 +0900

    refactor: 반복문 작성 방식 변경

--- 김은지/행렬의-곱셈.js
+++ 김은지/행렬의-곱셈.js
@@ -1,15 +1,3 @@
 function solution(arr1, arr2) {
-  const matrix = [];
-  for (let i = 0; i < arr1.length; i++) {
-    const row = [];
-    for (let j = 0; j < arr2[0].length; j++) {
-      let cell = 0;
-      for (let k = 0; k < arr1[i].length; k++) {
-        cell += arr1[i][k] * arr2[k][j];
-      }
-      row.push(cell);
-    }
-    matrix.push(row);
-  }
-  return matrix;
+  return arr1.map((row) => arr2[0].map((_, j) => row.reduce((sum, val, i) => sum + val * arr2[i][j], 0)));
 }
