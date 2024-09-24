2cffeaa632272e7920807f3e5498e0e5c0a4cbb1
publdaze <publdaze@naver.com>
2024-09-24 17:01:53 +0900

    refactor: 네이밍 문제에 더 적합하게 변경

--- 김은지/크레인-인형뽑기-게임.js
+++ 김은지/크레인-인형뽑기-게임.js
@@ -1,3 +1,35 @@
+// function setPositionGroups(board) {
+//   const N = board.length;
+//   const positionGroups = Array.from({ length: N + 1 }, () => []);
+
+//   for (let i = 0; i < N; i++) {
+//     for (let j = 0; j < N; j++) {
+//       if (board[j][i] === 0) continue;
+//       positionGroups[i + 1].push(board[j][i]);
+//     }
+//   }
+
+//   positionGroups.forEach((positionGroup) => positionGroup.reverse());
+
+//   return positionGroups;
+// }
+
+// function solution(board, moves) {
+//   const positionGroups = setPositionGroups(board);
+//   const stack = [];
+//   let bombPair = 0;
+//   for (const move of moves) {
+//     const currDoll = positionGroups[move].pop();
+//     if (stack.length > 0 && currDoll && stack.at(-1) === currDoll) {
+//       stack.pop();
+//       bombPair += 1;
+//       continue;
+//     }
+//     stack.push(currDoll);
+//   }
+//   return bombPair * 2;
+// }
+
 function setPositionGroups(board) {
   const N = board.length;
   const positionGroups = Array.from({ length: N + 1 }, () => []);
@@ -16,16 +48,16 @@ function setPositionGroups(board) {
 
 function solution(board, moves) {
   const positionGroups = setPositionGroups(board);
-  const stack = [];
+  const bucket = [];
   let bombPair = 0;
   for (const move of moves) {
     const currDoll = positionGroups[move].pop();
-    if (stack.length > 0 && currDoll && stack.at(-1) === currDoll) {
-      stack.pop();
+    if (bucket.length > 0 && currDoll && bucket.at(-1) === currDoll) {
+      bucket.pop();
       bombPair += 1;
       continue;
     }
-    stack.push(currDoll);
+    bucket.push(currDoll);
   }
   return bombPair * 2;
 }
