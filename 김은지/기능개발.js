e6cc3a8eac74830ff4e9badcd8f7cd2827b15551
publdaze <publdaze@naver.com>
2024-09-25 14:32:18 +0900

    refactor : O(N) 시간 복잡도로 개선

--- 김은지/기능개발.js
+++ 김은지/기능개발.js
@@ -1,4 +1,4 @@
-function solution(progresses, speeds) {
+/* function solution(progresses, speeds) {
   const completeTime = progresses.map((progress, i) => Math.ceil((100 - progress) / speeds[i]));
 
   let currDay = completeTime[0];
@@ -13,6 +13,27 @@ function solution(progresses, speeds) {
 
     deployCnts.push(cnt);
     currDay = completeTime[0];
+  }
+
+  return deployCnts;
+} */
+
+function solution(progresses, speeds) {
+  const completeTime = progresses.map((progress, i) => Math.ceil((100 - progress) / speeds[i]));
+
+  const deployCnts = [];
+  let currDay = completeTime[0];
+  let i = 0;
+  let cnt = 0;
+  while (i <= completeTime.length) {
+    if (currDay >= completeTime[i]) {
+      cnt++;
+    } else {
+      deployCnts.push(cnt);
+      cnt = 1;
+      currDay = completeTime[i];
+    }
+    i++;
   }
 
   return deployCnts;
