c4d424c74624fbe094444096cdd712adf4db56d3
publdaze <publdaze@naver.com>
2024-09-25 14:44:15 +0900

    refactor: currDay -> deployDay로 네이밍 변경

--- 김은지/기능개발.js
+++ 김은지/기능개발.js
@@ -21,19 +21,19 @@
 function solution(progresses, speeds) {
   const completeTimes = progresses.map((progress, i) => Math.ceil((100 - progress) / speeds[i]));
 
+  let deployDay = completeTimes[0];
   const deployGroupCnts = [];
-  let currDay = completeTimes[0];
   let deployGroupCnt = 0;
 
   for (const completeTime of completeTimes) {
-    if (currDay >= completeTime) {
+    if (deployDay >= completeTime) {
       deployGroupCnt++;
       continue;
     }
 
     deployGroupCnts.push(deployGroupCnt);
     deployGroupCnt = 1;
-    currDay = completeTime;
+    deployDay = completeTime;
   }
 
   deployGroupCnts.push(deployGroupCnt);
