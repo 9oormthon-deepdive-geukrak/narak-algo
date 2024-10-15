2b2e56c0c0a5c5321dcb046dbf1c407585506090
kkyung99 <becky0508@naver.com>
2024-10-15 16:04:38 +0900

    style: 띄어쓰기 변경

--- 김현경/피보나치_수.py
+++ 김현경/피보나치_수.py
@@ -1,8 +1,8 @@
 def solution(n):
     a, b = 0, 1 # a는 0번째 피보나치 수, b는 1번째 피보나치 수
     
-    for _ in range(2, n+1):
-        a, b = b, (a+b)% 1234567
+    for _ in range(2, n + 1):
+        a, b = b, (a + b) % 1234567
         
     return b
 
@@ -10,10 +10,10 @@ def solution(n):
 # def solution(n):
 #     answer = []
     
-#     for i in range(n+1):
+#     for i in range(n + 1):
 #         if i == 0 or i == 1:
 #             answer.append(i)
 #         else:
-#             answer.append((answer[i-1] + answer[i-2]) % 1234567)
+#             answer.append((answer[i - 1] + answer[i - 2]) % 1234567)
     
 #     return answer[-1]
