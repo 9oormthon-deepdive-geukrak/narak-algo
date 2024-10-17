9bc23622a05a494bddab0ac8dc738a739b635cd6
publdaze <publdaze@naver.com>
2024-10-17 15:00:30 +0900

    [LV2] 할인 행사

---
+++ 김은지/할인-행사.js
@@ -0,0 +1,28 @@
+const MEMBERSHIP_VALID_DAYS = 10;
+
+function solution(want, number, discount) {
+  let startDate = 0;
+  let endDate = startDate + MEMBERSHIP_VALID_DAYS;
+
+  const wantItems = want.reduce((acc, item, idx) => acc.set(item, number[idx]), new Map());
+  const currTermItems = discount
+    .slice(startDate, endDate)
+    .reduce((acc, item) => acc.set(item, (acc.get(item) || 0) + 1), new Map());
+
+  let cnt = 0;
+
+  while (startDate < endDate) {
+    if ([...wantItems.entries()].every(([key, value]) => currTermItems.get(key) >= value)) {
+      cnt++;
+    }
+
+    currTermItems.set(discount[startDate], currTermItems.get(discount[startDate]) - 1);
+    startDate += 1;
+    if (endDate < discount.length) {
+      currTermItems.set(discount[endDate], (currTermItems.get(discount[endDate]) || 0) + 1);
+      endDate += 1;
+    }
+  }
+
+  return cnt;
+}
