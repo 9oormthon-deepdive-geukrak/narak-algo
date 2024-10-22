555874de35539462efc3722e4f99770f4b69ead0
publdaze <publdaze@naver.com>
2024-10-22 14:15:34 +0900

    [LV2] 오픈채팅방

---
+++ 김은지/오픈채팅방.js
@@ -0,0 +1,19 @@
+const logMsg = {
+  Enter: (name) => `${name}님이 들어왔습니다.`,
+  Leave: (name) => `${name}님이 나갔습니다.`,
+};
+
+function solution(record) {
+  const members = record.reduce((acc, log) => {
+    const [, id, name] = log.split(" ");
+    if (name) acc[id] = name;
+    return acc;
+  }, {});
+
+  return record
+    .map((log) => {
+      const [command, id] = log.split(" ");
+      return logMsg[command]?.(members[id]);
+    })
+    .filter(Boolean);
+}
