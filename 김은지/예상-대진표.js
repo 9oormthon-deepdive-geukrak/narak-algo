ff454d1292253989a7ca9aee245ebe6efdf331d9
publdaze <publdaze@naver.com>
2024-10-29 14:39:12 +0900

    [LV2] 예상대진표

---
+++ 김은지/예상-대진표.js
@@ -0,0 +1,22 @@
+function solution(n, a, b) {
+  let round = 0;
+  while (a !== b) {
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    round += 1;
+  }
+  return round;
+}
+
+// function solution(n, a, b) {
+//     a -= 1;
+//     b -= 1;
+
+//     let round = 0;
+//     while (a !== b) {
+//         round += 1;
+//         a = Math.floor(a / 2);
+//         b = Math.floor(b / 2);
+//     }
+//     return round;
+// }
