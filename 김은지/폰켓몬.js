2ee8fcd53e0d8339dcec14daeedcbd0a75b57f76
publdaze <publdaze@naver.com>
2024-11-07 14:16:10 +0900

    [LV1] 폰켓몬

--- 김은지/폰켓몬.js
+++ 김은지/폰켓몬.js
@@ -1,10 +1,3 @@
 function solution(nums) {
-  const N = nums.length;
-
-  const type = nums.reduce((acc, num) => {
-    acc.set(num, (acc.get(num) || 0) + 1);
-    return acc;
-  }, new Map());
-
-  return Math.min(N / 2, type.size);
+  return Math.min(nums.length / 2, new Set(nums).size);
 }
