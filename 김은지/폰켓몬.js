d92692f09abb823d7d7d237eadd5eeb1fd2a00cd
publdaze <publdaze@naver.com>
2024-11-07 14:12:28 +0900

    [LV1] 폰켓몬

---
+++ 김은지/폰켓몬.js
@@ -0,0 +1,10 @@
+function solution(nums) {
+  const N = nums.length;
+
+  const type = nums.reduce((acc, num) => {
+    acc.set(num, (acc.get(num) || 0) + 1);
+    return acc;
+  }, new Map());
+
+  return Math.min(N / 2, type.size);
+}
