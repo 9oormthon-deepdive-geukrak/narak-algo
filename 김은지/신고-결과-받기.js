bfe40a883b4851edf5c48b19ce9b06bfacbca45b
publdaze <publdaze@naver.com>
2024-11-07 14:46:37 +0900

    [LV1] 신고 결과 받기

---
+++ 김은지/신고-결과-받기.js
@@ -0,0 +1,14 @@
+function solution(id_list, report, k) {
+  const froms = [
+    ...report.reduce((acc, log) => {
+      const [from, to] = log.split(" ");
+      if (!acc.has(to)) acc.set(to, new Set());
+      acc.get(to).add(from);
+      return acc;
+    }, new Map()),
+  ]
+    .filter(([_, value]) => value.size >= k)
+    .flatMap(([_, value]) => [...value]);
+
+  return id_list.map((id) => froms.filter((from) => from === id).length);
+}
